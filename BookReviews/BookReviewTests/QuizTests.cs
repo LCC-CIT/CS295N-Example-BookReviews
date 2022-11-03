b247d0d7932702f439dab355a2efdef8655c2d43
Brian Bird <birdb@lanecc.edu>
2022-11-03 14:51:25 -0700

    Added a test and better setup

--- BookReviews/BookReviewTests/QuizTests.cs
+++ BookReviews/BookReviewTests/QuizTests.cs
@@ -8,18 +8,21 @@ namespace BookReviewTests
 {
     public class QuizTests
     {
+        // For use by all tests:
+        const string WRONG_ANSWER = "wrong answer";
+        const string RIGHT_ANSWER1 = "Victor Hugo";
+        const string RIGHT_ANSWER2 = "1812";
+        const string RIGHT_ANSWER3 = "false";
+
         [Fact]
         public void CheckAnswerWrongTest()
         {
-            // arrange
-            string answer1 = "something";
-            string answer2 = "something";
-            string answer3 = "something";
+            // arrange: done at class scope
 
             // act
-           string result1 = Quiz.CheckAnswer(1, answer1);
-           string result2 = Quiz.CheckAnswer(2, answer2);
-           string result3 = Quiz.CheckAnswer(3, answer3);
+           string result1 = Quiz.CheckAnswer(1, WRONG_ANSWER);
+           string result2 = Quiz.CheckAnswer(2, WRONG_ANSWER);
+           string result3 = Quiz.CheckAnswer(3, WRONG_ANSWER);
 
             // assert
             Assert.True(result1.Equals(Quiz.WRONG));
@@ -30,20 +33,31 @@ namespace BookReviewTests
         [Fact]
         public void CheckAnswerRightTest()
         {
-            // arrange
-            string answer1 = "Victor Hugo";
-            string answer2 = "1812";
-            string answer3 = "false";
+            // arrange: done at class scope
 
             // act
-            string result1 = Quiz.CheckAnswer(1, answer1);
-            string result2 = Quiz.CheckAnswer(2, answer2);
-            string result3 = Quiz.CheckAnswer(3, answer3);
+            string result1 = Quiz.CheckAnswer(1, RIGHT_ANSWER1);
+            string result2 = Quiz.CheckAnswer(2, RIGHT_ANSWER2);
+            string result3 = Quiz.CheckAnswer(3, RIGHT_ANSWER3);
 
             // assert
             Assert.True(result1.Equals(Quiz.RIGHT));
             Assert.True(result2.Equals(Quiz.RIGHT));
             Assert.True(result3.Equals(Quiz.RIGHT));
+        }
+
+        [Fact]
+        public void CheckAnswerNotAQuestionTest()
+        {
+            // arrange: done at class scope
+
+            // act
+            string result1 = Quiz.CheckAnswer(0, RIGHT_ANSWER1);
+            string result2 = Quiz.CheckAnswer(4, RIGHT_ANSWER2);
+
+            // assert
+            Assert.True(result1.Equals(Quiz.NAQ));
+            Assert.True(result2.Equals(Quiz.NAQ));
         }
     }
 }
